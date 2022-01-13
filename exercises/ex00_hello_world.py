11e31b1e8d49c80724189ee9e4c3420e51981467
stephanieinteriano <sinteri@ad.unc.edu>
2022-01-13 17:04:17 -0500

    Finished Exercise 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,4 +1,6 @@
+"""My first program for COMP110.""" 
+
+__author__ = "730391348"
+
 print("Hello, world.")
 print("Hello, how is the world doing?")
-"""This is a Docstring.""" 
-__author__ = "730391348"
