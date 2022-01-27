396efaefcf9b2892e8b7b19a13aadfaa25e0f4fe
stephanieinteriano <sinteri@ad.unc.edu>
2022-01-26 20:24:56 -0500

    Progress on Exercise 1 - finally god damn

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,59 +1,45 @@
-"""EX01 - Chardle  - A cute step toward Wordle."""
+"""EX01 - Chardle - A cute step toward Wordle."""
+
 __author__ = "730391348"
 
-request: str = "Enter a 5-character word: "
-word_guess: str = "hellos"
-request_again: str = "Enter a single character: "
-letter_guess: str = "e"
+word_guess: str = input("Enter a 5-character word: ")
 
-if len(word_guess) != 5:
-    print(request + word_guess)
-    print("Error: Word must contain 5 characters")
-    exit()
+if len(word_guess) != 5:  
+    exit("Error: Word must contain 5 characters") 
 else: 
-    print(request + word_guess)
-    print(request_again + letter_guess)
-    if len(letter_guess) != 1:
-        print("Error: Character must be a single character")
-        exit()
-    else:
-        if letter_guess == " ":
-            print("Error: Character must be a single character")
-            exit()
-        else: 
-            if len(letter_guess) == 1:
-                print("Searching for " + letter_guess + " in " + word_guess)
-
-if letter_guess == word_guess[1]:
-    print(letter_guess + " is found at index 1") 
-else: 
-    if letter_guess == word_guess[0]:
-        print(letter_guess + " is found at index 0") 
-    else:
-        if letter_guess == word_guess[2]:
-            print(letter_guess + " is found at index 2")
-        else:
-            if letter_guess == word_guess[3]:
-                print(letter_guess + " is found at index 3")
-            else:
-                if letter_guess == word_guess[4]:
-                    print(letter_guess + " is found at index 4") 
-
+    letter_guess: str = input("Enter a single character: ")
 sub = letter_guess
-if word_guess.count(sub, 0, 5) == 1:
-    print("1 instance of " + sub + " is found in " + word_guess)
+
+if len(letter_guess) != 1:
+    print("Error: Character must be a single character.")
+    exit() 
 else:
-    if word_guess.count(sub, 0, 5) == 0:
-        print("No instances of " + sub + " found in " + word_guess)
-    else: 
-        if word_guess.count(sub, 0, 5) == 2:
-            print("2 instances of " + sub + " is found in " + word_guess) 
-        else: 
-            if word_guess.count(sub, 0, 5) == 3:
-                print("3 instances of " + sub + " is found in " + word_guess)
-            else: 
-                if word_guess.count(sub, 0, 5) == 4:
-                    print("4 instances of " + sub + " is found in " + word_guess)
-                else:
-                    if word_guess.count(sub, 0, 5) == 5:
-                        print("5 instances of " + sub + " is found in " + word_guess)
+    print("Searching for " + letter_guess + " in " + word_guess)
+
+if word_guess[0] == letter_guess:
+    print(letter_guess + " found at index 0")
+if word_guess[1] == letter_guess:
+    print(letter_guess + " found at index 1")
+if word_guess[2] == letter_guess:
+    print(letter_guess + " found at index 2")
+if word_guess[3] == letter_guess:
+    print(letter_guess + " found at index 3")
+if word_guess[4] == letter_guess:
+    print(letter_guess + " found at index 4")
+else:
+    exit  
+
+if word_guess.count(sub, 0, 5) == 1:
+    print("1 instance of " + letter_guess + " found in " + word_guess)
+if word_guess.count(sub, 0, 5) == 0:
+    print("No instances of " + letter_guess + " found in " + word_guess)
+if word_guess.count(sub, 0, 5) == 2:
+    print("2 instances of " + letter_guess + " found in " + word_guess) 
+if word_guess.count(sub, 0, 5) == 3:
+    print("3 instances of " + letter_guess + " found in " + word_guess)
+if word_guess.count(sub, 0, 5) == 4:
+    print("4 instances of " + letter_guess + " found in " + word_guess)
+if word_guess.count(sub, 0, 5) == 5:
+    print("5 instances of " + letter_guess + " found in " + word_guess)
+else: 
+    exit 
